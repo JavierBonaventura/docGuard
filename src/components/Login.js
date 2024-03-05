21084d80f93764bccfb3a66515b0c7725cca421f
Javier Bonaventura <bonaventura.javier@gmail.com>
2024-03-05 20:22:50 +0100

    modificas 05 marzoo 2024

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
+import { FiAlertTriangle } from "react-icons/fi";
+
+
 const LoginPage = ({ onLogin }) => {
     const [formData, setFormData] = useState({
         username: '',
         password: ''
     });
+
+    const [showErrorPopup, setShowErrorPopup] = useState(false); // Estado para controlar la visibilidad del popup de error
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -23,25 +28,23 @@ const LoginPage = ({ onLogin }) => {
             });
             if (response.ok) {
                 onLogin(formData.username);
-
                 console.log('Login successful');
             } else {
                 console.error('Login failed');
+                setShowErrorPopup(true); // Mostrar el popup de error si el inicio de sesión falla
             }
         } catch (error) {
             console.error('Error during login:', error);
         }
     };
 
-
     useEffect(() => {
         const video = document.getElementById('background-video');
-        video.playbackRate = 0.6; // Cambia 0.5 al valor que desees para ajustar la velocidad de reproducción
+        video.playbackRate = 0.6;
     }, []);
 
     return (
         <div className="font-sans relative">
-            {/* Contenedor del video */}
             <div className="video-container absolute inset-0 z-0">
                 <video autoPlay muted loop id="background-video" className="w-full h-full object-cover">
                     <source src="/videos/video.mp4" type="video/mp4" />
@@ -49,14 +52,13 @@ const LoginPage = ({ onLogin }) => {
                 </video>
             </div>
 
-            {/* Contenido de la página */}
             <div className="relative flex items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8 z-10">
                 <div className="max-w-sm w-full space-y-8">
-                    <div className="login-box"> {/* Contenedor del formulario de login */}
+                    <div className="login-box">
                         <h2 className="text-2xl font-bold mb-1">Welcome Back!</h2>
                         <h3 className="text-lg mb-4">Please sign in to your account</h3>
                         <form onSubmit={handleSubmit}>
-                        <div className="mb-4">
+                            <div className="mb-4">
                                 <label htmlFor="username" className="block text-base font-medium text-black">Username</label>
                                 <input
                                     type="text"
@@ -88,19 +90,27 @@ const LoginPage = ({ onLogin }) => {
                                         Sign In
                                     </button>
                                 </div>
-                                {/* <div className="mb-4">
-                                    <Link to="/register" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
-                                        Register
-                                    </Link>
-                                </div>
-                                <div>
-                                    <a href="#" className="text-sm text-black font-medium hover:text-gray-900">Forgot your password?</a>
-                                </div> */}
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
+            {showErrorPopup && (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-20">
+        <div className="absolute bg-gray-100 border border-gray-300 rounded-lg px-6 py-8 shadow-lg flex items-center">
+          
+            <div>
+                <h2 className="text-lg font-semibold mb-2 text-red-600 flex items-center">
+                <FiAlertTriangle className="text-4xl text-red-600 mr-2" />
+                    <span className="mr-2 text-lg">Login Failed</span>
+                </h2>
+                <p className="text-sm">Incorrect username or password. Please try again.</p>
+                <button onClick={() => setShowErrorPopup(false)} className="mt-4 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-6 rounded">OK</button>
+            </div>
+        </div>
+    </div>
+)}
+
         </div>
     );
 };
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -10,8 +10,6 @@ const UserCRUD = ({ onCreateUserClick, onEditUserClick }) => {
   const [roles, setRoles] = useState([]);
   const [accounts, setAccounts] = useState([]);
   const [statuses, setStatuses] = useState([]);
-
-
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
@@ -38,12 +36,21 @@ const UserCRUD = ({ onCreateUserClick, onEditUserClick }) => {
   }, []);
 
 
-  const deleteUser = async (userId) => {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [userIdToDelete, setUserIdToDelete] = useState(null);
+
+  const toggleDeleteConfirmation = (userId = null) => {
+    setShowDeleteConfirmation(!showDeleteConfirmation);
+    setUserIdToDelete(userId);
+  };
+
+  const deleteUser = async () => {
     try {
-      await axios.delete(`/api/v1/user/${userId}`);
+      await axios.delete(`/api/v1/user/${userIdToDelete}`);
       // Actualizar la lista de usuarios después de eliminar uno
       const response = await axios.get("/api/v1/users");
       setUsers(response.data);
+      toggleDeleteConfirmation(); // Ocultar el popup de confirmación después de eliminar
     } catch (error) {
       console.error("Error deleting user:", error);
     }
@@ -54,7 +61,6 @@ const UserCRUD = ({ onCreateUserClick, onEditUserClick }) => {
     // Puedes usar navigate o simplemente cambiar la URL
   };
 
-
   const filteredUsers = users.filter(
     (user) =>
       user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -62,34 +68,33 @@ const UserCRUD = ({ onCreateUserClick, onEditUserClick }) => {
       user.email.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-// Función para obtener el nombre del rol según el ID
-const getRoleName = (roleId) => {
-  if (roles.length === 0) return "Unknown";
-  const role = roles.find((r) => r.roleid === roleId);
-  return role ? role.rolename : "Unknown";
-};
+  // Función para obtener el nombre del rol según el ID
+  const getRoleName = (roleId) => {
+    if (roles.length === 0) return "Unknown";
+    const role = roles.find((r) => r.roleid === roleId);
+    return role ? role.rolename : "Unknown";
+  };
 
-// Función para obtener el nombre del account según el ID
-const getAccountName = (accountId) => {
-  if (accounts.length === 0) return "Unknown";
-  const account = accounts.find((r) => r.accountid === accountId);
-  return account ? account.accountname : "Unknown";
-};
+  // Función para obtener el nombre del account según el ID
+  const getAccountName = (accountId) => {
+    if (accounts.length === 0) return "Unknown";
+    const account = accounts.find((r) => r.accountid === accountId);
+    return account ? account.accountname : "Unknown";
+  };
 
-// Función para obtener el nombre del status según el ID
-const getStatusName = (statusId) => {
-  if (statuses.length === 0) return "Unknown";
-  const status = statuses.find((r) => r.statusid === statusId);
-  return status ? status.statusname : "Unknown";
-};
-
+  // Función para obtener el nombre del status según el ID
+  const getStatusName = (statusId) => {
+    if (statuses.length === 0) return "Unknown";
+    const status = statuses.find((r) => r.statusid === statusId);
+    return status ? status.statusname : "Unknown";
+  };
 
   return (
-    <div className="container mx-auto  bg-white shadow-lg">
+    <div className="container mx-auto  bg-white shadow-lg p-2">
       <div className="flex mb-4  ">
         <input
           type="text"
-          className="border rounded py-2 px-1 w-3/5 mr-2"
+          className="border rounded py-2 px-1 w-2/5 mr-2"
           placeholder="Buscar usuario..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
@@ -125,27 +130,45 @@ const getStatusName = (statusId) => {
               <td className="border px-1 py-2">{user.password}</td>
               <td className="border px-1 py-2">{getStatusName(user.statusid)}</td>
               <td className="border px-1 py-2 ">
-                <div className="flex justify-center"> 
+                <div className="flex justify-center">
                   {/* Botones de acciones */}
                   <button
-                  onClick={() => editUser(user.userid)}
-                  className="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded mr-2"
-                >
-                  Editar
-                </button>
-                <button
-                  onClick={() => deleteUser(user.userid)}
-                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded"
-                >
-                  Eliminar
-                </button>
+                    onClick={() => editUser(user.userid)}
+                    className="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded mr-2"
+                  >
+                    Editar
+                  </button>
+                  <button
+                    onClick={() => toggleDeleteConfirmation(user.userid)}
+                    className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded"
+                  >
+                    Eliminar
+                  </button>
                 </div>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+        {/* Popup de confirmación */}
+        {showDeleteConfirmation && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-4 rounded-lg py-12 px-12">
+            <p>¿Estás seguro de que deseas eliminar este usuario?</p>
+            <div className="flex justify-end mt-4">
+              <button onClick={deleteUser} className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-5 rounded mr-2">
+                Sí
+              </button>
+              <button onClick={toggleDeleteConfirmation} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-4 rounded">
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
+
+    
   );
 };
 
